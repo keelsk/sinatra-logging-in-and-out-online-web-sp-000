364c7b975858bc6814fe69b9ca63ab20f18e2d41
Kirsten Keels <keelsk@gmail.com>
2020-03-30 02:32:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,8 +23,6 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    
-    # @user = User.find_by_id(session[:user_id])
     if Helpers.is_logged_in?(session)
       @user = Helpers.current_user(session)
       erb :account
