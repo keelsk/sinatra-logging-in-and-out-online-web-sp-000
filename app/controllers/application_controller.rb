81e8deec03366d894b597bb1f10167dfba72398e
Kirsten Keels <keelsk@gmail.com>
2020-03-30 02:26:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
 
-    @user = User.find_by_id(session[:user_id])
+    # @user = User.find_by_id(session[:user_id])
     if @user
       erb :account
     else
